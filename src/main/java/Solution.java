04d7ab29aead0cf27b75ef2b3a3695576d474e7e
Mauricio MdS <45921381+Mauricio-MdS@users.noreply.github.com>
2024-09-20 20:42:03 -0300

    Implements singleNumber

---
+++ src/main/java/Solution.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int singleNumber = 0;
+
+        for (int number : nums) singleNumber = singleNumber ^ number;
+
+        return singleNumber;
+    }
+}
